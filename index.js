c480c1fc74250f95d6697c1fc101ee66303ab8ef
Liam Keery <l_keery@fanshaweonline.ca>
2020-01-22 13:51:57 -0500

    Heroku config

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use('/css', express.static(__dirname + '/css'))
+app.use('/js', express.static(__dirname + '/js'))
+app.use('/bower_components', express.static(__dirname + '/bower_components'))
+
+app.get('/', function (request, response) {
+    response.sendFile('/', { root: __dirname })
+})
+
+app.listen(app.get('port'), function () {
+    console.log("Node app is running at localhost:" + app.get('port'))
+})
