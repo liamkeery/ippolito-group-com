619bac0bf542bbad7e7e9c4912d25f6233ace857
Liam Keery <l_keery@fanshaweonline.ca>
2020-02-19 16:09:22 -0500

    Bug fixes, content input

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 $(document).foundation();
+
+console.log('Connected');
+
+$(function() {
+	$(window).scroll(function(){
+		var scrollTop = $(window).scrollTop();
+		if(scrollTop != 0)
+			$('.top-bar').stop().animate({'opacity':'0.2'},400);
+		else	
+			$('.top-bar').stop().animate({'opacity':'1'},400);
+	});
+	
+	$('.top-bar').hover(
+		function (e) {
+			var scrollTop = $(window).scrollTop();
+			if(scrollTop != 0){
+				$('.top-bar').stop().animate({'opacity':'1'},400);
+			}
+		},
+		function (e) {
+			var scrollTop = $(window).scrollTop();
+			if(scrollTop != 0){
+				$('.top-bar').stop().animate({'opacity':'0.2'},400);
+			}
+		}
+	);
+});
